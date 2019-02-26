82941983c79c4854196f2d8c6251e5fd60918d67
rafamolino <Xxracso40xX@gmail.com>
2019-02-26 13:56:37 +0100

    Corrección de errores closes #36

--- app/src/main/java/com/example/practicatema7pmdm/NuevoEdicion.java
+++ app/src/main/java/com/example/practicatema7pmdm/NuevoEdicion.java
@@ -1,7 +1,11 @@
 package com.example.practicatema7pmdm;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -15,5 +19,19 @@ public class NuevoEdicion extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+    }
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuBuilder m = (MenuBuilder) menu;
+        m.setOptionalIconsVisible(true);
+        return true;
+    }
+    @SuppressLint("NewApi")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        return false;
     }
 }
