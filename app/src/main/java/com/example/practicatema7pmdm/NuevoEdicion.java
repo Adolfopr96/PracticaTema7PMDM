653cc94acb405d3fdb1f2f157c645a2c97860e1c
rafamolino <Xxracso40xX@gmail.com>
2019-02-26 14:26:28 +0100

    Adelanto Botón guardar pantalla Nuevo

--- app/src/main/java/com/example/practicatema7pmdm/NuevoEdicion.java
+++ app/src/main/java/com/example/practicatema7pmdm/NuevoEdicion.java
@@ -6,16 +6,29 @@ import android.os.Bundle;
 import android.support.v7.view.menu.MenuBuilder;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 
-public class NuevoEdicion extends AppCompatActivity {
+import com.example.practicatema7pmdm.Logic.LogicLugar;
 
+public class NuevoEdicion extends AppCompatActivity {
+    EditText edit1, edit2, edit3, edit4;
+    RatingBar rb;
+    Spinner spinner;
+    String texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_edicion);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+        edit1 = findViewById(R.id.editText);
+        edit2 = findViewById(R.id.editText2);
+        edit3 = findViewById(R.id.editText3);
+        edit4 = findViewById(R.id.editText4);
+        spinner =findViewById(R.id.spinner2);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -32,6 +45,36 @@ public class NuevoEdicion extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
+        int categoria=0;
+        texto = spinner.getSelectedItem().toString();
+        if(texto=="Comida Mexicana")
+        {
+            categoria=1;
+        }
+        else if(texto=="Comida Francesa")
+        {
+            categoria=2;
+        }
+        else if(texto=="Comida Española")
+        {
+            categoria=3;
+        }
+        else if(texto=="Comida Coreana")
+        {
+            categoria=4;
+        }
+        //Comida India
+        else
+        {
+            categoria=5;
+        }
+        App.lugarActivo.setNombre(edit1.getText().toString());
+        App.lugarActivo.setLatitud(Float.parseFloat(edit3.getText().toString()));
+        App.lugarActivo.setLongitud(Float.parseFloat(edit2.getText().toString()));
+        App.lugarActivo.setComentarios(edit4.getText().toString());
+        App.lugarActivo.setValoracion(rb.getRating());
+        App.lugarActivo.setCategoria(categoria);
+        LogicLugar.insertarLugar(this, App.lugarActivo );
         return false;
     }
 }
