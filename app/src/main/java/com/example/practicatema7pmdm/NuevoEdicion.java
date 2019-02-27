599e101f446834332a9c6daaa8481170a085f875
rafamolino <Xxracso40xX@gmail.com>
2019-02-27 09:48:52 +0100

    Funcionamiento de la pantalla "NuevoEdicion" en el momento de generar un nuevo lugar. closes #40, closes #39

--- app/src/main/java/com/example/practicatema7pmdm/NuevoEdicion.java
+++ app/src/main/java/com/example/practicatema7pmdm/NuevoEdicion.java
@@ -12,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.practicatema7pmdm.Logic.LogicLugar;
 
@@ -29,6 +30,7 @@ public class NuevoEdicion extends AppCompatActivity {
         edit3 = findViewById(R.id.editText3);
         edit4 = findViewById(R.id.editText4);
         spinner =findViewById(R.id.spinner2);
+        rb = findViewById(R.id.ratingBar2);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -46,7 +48,7 @@ public class NuevoEdicion extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item)
     {
         int categoria=0;
-        texto = spinner.getSelectedItem().toString();
+        /*texto = spinner.getSelectedItem().toString();
         if(texto=="Comida Mexicana")
         {
             categoria=1;
@@ -68,13 +70,15 @@ public class NuevoEdicion extends AppCompatActivity {
         {
             categoria=5;
         }
+        */
         App.lugarActivo.setNombre(edit1.getText().toString());
         App.lugarActivo.setLatitud(Float.parseFloat(edit3.getText().toString()));
         App.lugarActivo.setLongitud(Float.parseFloat(edit2.getText().toString()));
         App.lugarActivo.setComentarios(edit4.getText().toString());
         App.lugarActivo.setValoracion(rb.getRating());
-        App.lugarActivo.setCategoria(categoria);
+        App.lugarActivo.setCategoria(spinner.getSelectedItemPosition());
         LogicLugar.insertarLugar(this, App.lugarActivo );
+        Toast.makeText(this, getResources().getString(R.string.toast1), Toast.LENGTH_SHORT).show();
         return false;
     }
 }
