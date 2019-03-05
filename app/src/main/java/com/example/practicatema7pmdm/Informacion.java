f05f58ebf82762d318db4c2f72d470946f4b8e27
rafamolino <Xxracso40xX@gmail.com>
2019-03-05 09:27:04 +0100

    Creación de los elementos de la pantalla información incluyendo el menú del mismo. closes #35, closes #24, closes #25

--- app/src/main/java/com/example/practicatema7pmdm/Informacion.java
+++ app/src/main/java/com/example/practicatema7pmdm/Informacion.java
@@ -1,7 +1,14 @@
 package com.example.practicatema7pmdm;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.practicatema7pmdm.Logic.LogicLugar;
 
 public class Informacion extends AppCompatActivity {
 
@@ -11,5 +18,20 @@ public class Informacion extends AppCompatActivity {
         setContentView(R.layout.activity_informacion);
         //Pondremos SALIDAINFORMACION=1 PARA IDENTIFICAR EN QUE MOMENTO HEMOS ENTRADO POR INFORMACIÓN EN VEZ DE NUEVO.
         //App.SALIDAINFORMACION=1;
+    }
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuinformacion, menu);
+        MenuBuilder m = (MenuBuilder) menu;
+        m.setOptionalIconsVisible(true);
+        return true;
+    }
+    @SuppressLint("NewApi")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        return false;
     }
 }
